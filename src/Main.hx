bfb21090781ef3934d416f9955558c53cfbbeace
Ziruak <42813205+Ziruak@users.noreply.github.com>
2022-06-20 17:54:52 +0700

    Stop music playing at start

--- src/Main.hx
+++ src/Main.hx
@@ -107,7 +107,7 @@ var txtInpCellSize:h2d.TextInput;
             Values.cells[i[0]*Values.cellsJ+i[1]].updateState(true);
 
 		initToolBar();
-		Res.gspd2.play(true,0.3);
+		//Res.gspd2.play(true,0.3);
         }
 
     override  function update(dt:Float) {
